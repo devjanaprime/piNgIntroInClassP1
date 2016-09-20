237e4cd5fea08f828f03fb74b8d8926960aca1cc
devjanaprime <dev@primeacademy.io>
2016-09-20 10:44:08 -0500

    twoWayBind

--- scripts/angularIntro.js
+++ scripts/angularIntro.js
@@ -4,4 +4,6 @@ var myApp = angular.module( 'myApp', [] );
 
 myApp.controller( 'commanderDreamboat', [ '$scope', function( $scope ){
   console.log( 'NG' );
+  var protonTorpedoes = 12;
+  console.log( 'protonTorpedoes:', protonTorpedoes );
 }]); // end controller
