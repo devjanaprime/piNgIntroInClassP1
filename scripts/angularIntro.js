54675e52253984d548e3dee7bd3a4f6b70a05be8
devjanaprime <dev@primeacademy.io>
2016-09-20 10:50:00 -0500

    ngClickModelFunction

--- scripts/angularIntro.js
+++ scripts/angularIntro.js
@@ -6,4 +6,10 @@ myApp.controller( 'commanderDreamboat', [ '$scope', function( $scope ){
   console.log( 'NG' );
   var protonTorpedoes = 12;
   console.log( 'protonTorpedoes:', protonTorpedoes );
+
+  $scope.getCrystals = function(){
+    console.log( 'in getCrystals' );
+    console.log( 'crystals as input:', $scope.crystals );
+  } // end getCrystals
+
 }]); // end controller
